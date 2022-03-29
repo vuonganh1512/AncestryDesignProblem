1daddf01a3d9a33e6686f3120f76a94ba875f05e
Andree Vuong <93449196+vuonganh1512@users.noreply.github.com>
2022-03-28 20:40:49 -0500

    Hello
    Fixed

--- src/dna/DnaSequence.java
+++ src/dna/DnaSequence.java
@@ -14,7 +14,7 @@ public class DnaSequence {
         // TODO: fix me
         List<String> aminoAcids = new LinkedList<>();
         int i = 0;
-        while(i < dna.length()-3) {
+        while(i < dna.length()-2) {
             String triplet = "" + dna.charAt(i) + dna.charAt(i+1) + dna.charAt(i+2);
             try {
                 String acid = this.dnaCodon.acidFor(triplet);
--- src/dna/DnaSequenceTest.java
+++ src/dna/DnaSequenceTest.java
@@ -3,8 +3,6 @@ package dna;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class DnaSequenceTest {
     DnaSequence dnaSequence;
     @Before
